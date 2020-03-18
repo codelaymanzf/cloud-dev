24ddf5bf0fe6374d45803542d0e3eb2fb3ae325e
zhangfeng <zhangfeng@goumin.com>
2020-03-18 20:45:21 +0800

    update book module

--- cloudfunctions/getisbn/index.js
+++ cloudfunctions/getisbn/index.js
@@ -1,16 +1,26 @@
 // 云函数入口文件
-const cloud = require('wx-server-sdk')
+const cloud = require("wx-server-sdk");
+const axios = require("axios");
+const doubanbook = require("doubanbook"); // 用于解密加密的图书信息
 
-cloud.init()
+cloud.init();
+
+async function getDoubanBookBrief(isbn) {
+  const url =
+    "https://search.douban.com/book/subject_search?search_text=" + isbn;
+  let res = await axios.get(url);
+  let reg = /window\.__DATA__ = "(.*)"/;
+
+  if (reg.test(res.data)) {
+    let searchData = doubanbook(RegExp.$1)[0];
+    return searchData;
+  }
+}
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+  let { isbn } = event;
+  let bookBrief = await getDoubanBookBrief(isbn);
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
-}
+  return bookBrief;
+};
--- miniprogram/pages/book/book.js
+++ miniprogram/pages/book/book.js
@@ -1,19 +1,17 @@
 // pages/book/book.js
-Page({
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    bookBrief: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {},
   // takePhoto() {
   //   const ctx = wx.createCameraContext()
   //   ctx.takePhoto({
@@ -26,59 +24,82 @@ Page({
   //   })
   // },
   scanCode() {
-    console.log('begin scan')
     wx.scanCode({
-      succcess: res => {
-        console.log('res', res.result)
+      success: res => {
+        wx.showLoading({
+          title: '加载中'
+        });
+        console.log("res", res);
+
+        wx.cloud.callFunction({
+          name: "getisbn",
+          data: {
+            isbn: res.result
+          }
+          // succcess: res => {
+          //   console.log("success", res);
+          //   wx.hideLoading();
+          // },
+          // fail: err => {
+          //   console.log("err", err);
+          // },
+          // complete: () => {}
+        }).then(res => {
+          console.log('success', res);
+          this.setData({
+            bookBrief: res.result
+          })
+          wx.hideLoading();
+        }).catch(err => {
+          console.log('err', err);
+          wx.hideLoading();
+        });
+      },
+      fail: err => {
+        wx.showToast({
+          title: "扫码失败，请稍后再试"
+        });
       }
+    });
+  },
+
+  checkDetail() {
+    wx.navigateTo({
+      url: '/pages/h5/h5?url=' + this.data.bookBrief.id
     })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
